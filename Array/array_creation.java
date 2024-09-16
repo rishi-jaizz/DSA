bc7e609570e8e5ea2ee15f07819e786da72660aa
rishi-jaizz <rishijaiswal888@gmail.com>
2024-09-17 00:15:22 +0530

    arrays creation

---
+++ Array/array_creation.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class array_creation {
+    
+// dataType arrayName[]= new dataType[size];
+
+// creating an array
+int marks[] = new int[50];
+int numbers[]={1,2,3};
+int moreNumbers[]={4,5,6};
+String fruits[]={"apple","mango","orange"};
+
+}
+
+// List of Elements of the same type placed in a contiguous memory location.
